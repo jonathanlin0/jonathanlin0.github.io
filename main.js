d608a6951db212246fc91dc950008532bed55ea2
Jonathan Lin <67934523+jonathanlin0@users.noreply.github.com>
2022-12-05 00:17:26 -0800

    commented out ip fetcher request

--- main.js
+++ main.js
@@ -4,19 +4,19 @@ function ip2int(ip) {
     return ip.split('.').reduce(function(ipInt, octet) { return (ipInt<<8) + parseInt(octet, 10)}, 0) >>> 0;
 }
 
-$.ajax({
-    type: 'GET',
-    dataType:'json',
-    async:false,
-    url:'https://httpbin.org/ip',
-    success: function(responseData) {
-        ip = ip2int(responseData.origin);
-        console.log(ip)
-    },
-    error: function(XMLHttpRequest, textStatus, errorThrown){
-        console.log('Error');
-    }
-})
+// $.ajax({
+//     type: 'GET',
+//     dataType:'json',
+//     async:false,
+//     url:'https://httpbin.org/ip',
+//     success: function(responseData) {
+//         ip = ip2int(responseData.origin);
+//         console.log(ip)
+//     },
+//     error: function(XMLHttpRequest, textStatus, errorThrown){
+//         console.log('Error');
+//     }
+// })
 
 window.onload = function() {
 
